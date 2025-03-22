0a41a2380340a77bef3ceca3233f010c195ec189
Silvia Indrigo <silviaindrigo@gmail.com>
2025-03-21 22:05:32 -0300

    criando variaveis

--- 1- Pacotes/go.mod
+++ 1- Pacotes/go.mod
@@ -2,3 +2,4 @@ module modulo
 
 go 1.24.1
 
+require github.com/badoux/checkmail v1.2.4
--- 1- Pacotes/main.go
+++ 1- Pacotes/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("escrevendo do arquivo main!")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("silviaindriug8yuh")
+	fmt.Println(erro)
 	
 }
